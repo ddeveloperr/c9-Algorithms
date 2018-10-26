2b05bffcf0b9ed415b989de418ab0ce77c052e82
Ismail Cucevic <cucevicismail@gmail.com>
2018-10-26 08:29:55 +0000

    add Refactored solution

--- SlidingWindow.js
+++ SlidingWindow.js
@@ -1,19 +1,37 @@
 //This is a function called maxSubarraySum which accepts an array on integers and a number called n. 
 //The funtion should calculate the maximum sum of n consecutive elements in the array.
-function maxSubarraySum(arr,num){
-    if(num>arr.length){
+function maxSubarraySum(arr, num){
+    if(num > arr.length){
         return null;
     }
     var max = -Infinity;
-    for(let i=0; i < arr.length - num +1; i++){
+    for(let i=0; i < arr.length - num + 1; i++){
         var temp = 0;
-        for(let j = 0; j<num; j++){
-            temp +=arr[i+j]; 
+        for(let j = 0; j < num; j++){
+            temp += arr[i + j]; 
         }
-        if(temp >max){
-            max= temp;
+        if(temp > max){
+            max = temp;
         }
     }
     return max;
 }
 maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+//time complexity is O(n^2)
+
+// Refactor
+function maxSubarraySum(arr,num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+    for(let i=0; i<num; i++){
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for(let i = num; i< arr.length; i++){
+        tempSum = tempSum - arr[i-num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+// Time Complexity - O(n)
