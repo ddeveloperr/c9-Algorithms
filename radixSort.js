a8a9b1f473c1a7986d95318d3c300ae8004e9126
Ismail Cucevic <cucevicismail@gmail.com>
2018-10-30 14:00:04 +0000

    add radix-Sort

---
+++ radixSort.js
@@ -0,0 +1,35 @@
+// Redix Sort
+function getDigit(num,i){
+    return Math.floor(Math.abs(num)/Math.pow(10,i)) % 10;
+}
+function digitCount(num){
+    if(num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+    
+}
+function mostDigits(nums){
+    let maxDigits = 0;
+    for(let i = 0; i< nums.length; i++){
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+}
+function radixSort(nums){
+ let maxDigitCount = mostDigits(nums);
+ for(let k=0; k<maxDigitCount; k++){
+     let digitBuckets = Array.from({length:10}, () => []);
+     for(let i=0; i<nums.length; i++){
+         let digit = getDigit(nums[i],k);
+             digitBuckets[digit].push(nums[i]);
+     }
+     nums=[].concat(...digitBuckets);
+ }
+ return nums;
+}
+
+radixSort([12,45,67,34,1246,1234,3]);
+
+// Best case for Time Complexity is         O(nk)   - n --> length of array, k--> number of digits(avarage)
+// Average case for Time Complexity is      O(nk)
+// Worst case for Time Comlexity is         O(nk)
+// Space complexity is                      O(n + k) 
